10548b5ea9beda05637eb00c588059f1b34dda23
dmitry.razumov.nb <dmitry.razumov@novsu.ru>
2023-08-09 00:40:41 +0300

    refact: add _request(url, options) в Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -20,7 +20,7 @@ export default class Api {
 
   // метод - запрос данных юзера с сервера
   getUserData() {
-    this._request(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       method: 'GET',
       headers: this._headers
     });
@@ -28,7 +28,7 @@ export default class Api {
 
   // метод - запрос первичных карточек с сервера
   getInitialCards() {
-    this._request(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       method: 'GET',
       headers: this._headers
     })
@@ -36,7 +36,7 @@ export default class Api {
 
   // метод - запрос на обновление данных юзера на сервере
   editAuthorData(newUserData) {
-    this._request(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
       headers: this._headers,
       body: JSON.stringify({
@@ -48,7 +48,7 @@ export default class Api {
   
   // метод - запрос на обновление аватарки юзера 
   editAuthorAvatar(newUserAvatar) {
-    this._request(`${this._baseUrl}/users/me/avatar`, {
+    return this._request(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
       headers: this._headers,
       body: JSON.stringify({
@@ -59,7 +59,7 @@ export default class Api {
 
   // метод - запрос на отправку своей карточки на сервер
   postCustomCard(cardToAdd) {
-    this._request(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       method: 'POST',
       headers: this._headers,
       body: JSON.stringify({
@@ -71,7 +71,7 @@ export default class Api {
 
   // метод - запрос на удаление карточки
   deleteCard(cardToDeleteId) {
-    this._request(`${this._baseUrl}/cards/${cardToDeleteId}`, {
+    return this._request(`${this._baseUrl}/cards/${cardToDeleteId}`, {
       method: 'DELETE',
       headers: this._headers
     })
@@ -79,7 +79,7 @@ export default class Api {
 
   // метод - запрос на поставить лайк карточки
   likeCard(cardToLikeId) {
-    this._request(`${this._baseUrl}/cards/likes/${cardToLikeId}`, {
+    return this._request(`${this._baseUrl}/cards/likes/${cardToLikeId}`, {
       method: 'PUT',
       headers: this._headers
     })
@@ -87,7 +87,7 @@ export default class Api {
 
   // метод - запрос на удалить лайк карточки
   unlikeCard(cardToUnlikeId) {
-    this._request(`${this._baseUrl}/cards/likes/${cardToUnlikeId}`, {
+    return this._request(`${this._baseUrl}/cards/likes/${cardToUnlikeId}`, {
       method: 'DELETE',
       headers: this._headers
     })
